1e1b4b8f4ce95de85d4c67f9ac2d9646d25f2347
Aleh Pilets <pilets.oleg@gmail.com>
2025-01-16 15:09:14 +0100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Management;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Waiting for the change... Click ENTER to exit.");
+
+        ManagementEventWatcher connectWatcher = new ManagementEventWatcher();
+        connectWatcher.Query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+        connectWatcher.EventArrived += (sender, eventArgs) =>
+        {
+           Console.Clear();
+            Console.WriteLine($"USB device is connected. \n\nClick ENTER to exit.");
+        };
+        connectWatcher.Start();
+
+        ManagementEventWatcher disconnectWatcher = new ManagementEventWatcher();
+        disconnectWatcher.Query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 3");
+        disconnectWatcher.EventArrived += (sender, eventArgs) =>
+        {
+           Console.Clear();
+            Console.WriteLine($"USB device is disconnected. \n\nClick ENTER to exit.");
+        };
+        disconnectWatcher.Start();
+
+        Console.ReadLine();
+
+        connectWatcher.Stop();
+        disconnectWatcher.Stop();
+        connectWatcher.Dispose();
+        disconnectWatcher.Dispose();
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("usb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("usb")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("53ec10b3-e0b2-4335-a458-d4f0c4c45b0e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
